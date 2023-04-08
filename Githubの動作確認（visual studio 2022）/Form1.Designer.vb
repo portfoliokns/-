ef312b096ef38085ce073f302a8fa42cee929857
konishi33 <100762595+konishi33@users.noreply.github.com>
2023-04-08 23:34:31 +0900

    ログイン画面上に各種オブジェクトを配置

--- Githubの動作確認（visual studio 2022）/Form1.Designer.vb
+++ Githubの動作確認（visual studio 2022）/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class frmLoginScreen
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,15 +22,85 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        lblUserID = New Label()
+        lblPassword = New Label()
+        txtUserID = New TextBox()
+        txtPassword = New TextBox()
+        btnLogin = New Button()
+        btnClose = New Button()
         SuspendLayout()
         ' 
-        ' Form1
+        ' lblUserID
+        ' 
+        lblUserID.AutoSize = True
+        lblUserID.Location = New Point(120, 47)
+        lblUserID.Name = "lblUserID"
+        lblUserID.Size = New Size(66, 15)
+        lblUserID.TabIndex = 0
+        lblUserID.Text = "ユーザーID："
+        ' 
+        ' lblPassword
+        ' 
+        lblPassword.AutoSize = True
+        lblPassword.Location = New Point(120, 104)
+        lblPassword.Name = "lblPassword"
+        lblPassword.Size = New Size(63, 15)
+        lblPassword.TabIndex = 1
+        lblPassword.Text = "パスワード："
+        ' 
+        ' txtUserID
+        ' 
+        txtUserID.Location = New Point(215, 39)
+        txtUserID.Name = "txtUserID"
+        txtUserID.Size = New Size(100, 23)
+        txtUserID.TabIndex = 2
+        ' 
+        ' txtPassword
+        ' 
+        txtPassword.Location = New Point(215, 101)
+        txtPassword.Name = "txtPassword"
+        txtPassword.Size = New Size(100, 23)
+        txtPassword.TabIndex = 3
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.Location = New Point(109, 165)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(75, 23)
+        btnLogin.TabIndex = 4
+        btnLogin.Text = "ログイン"
+        btnLogin.UseVisualStyleBackColor = True
+        ' 
+        ' btnClose
+        ' 
+        btnClose.Location = New Point(230, 165)
+        btnClose.Name = "btnClose"
+        btnClose.Size = New Size(75, 23)
+        btnClose.TabIndex = 5
+        btnClose.Text = "閉じる"
+        btnClose.UseVisualStyleBackColor = True
+        ' 
+        ' frmLoginScreen
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Name = "Form1"
-        Text = "フォーム名を変更"
+        ClientSize = New Size(407, 230)
+        Controls.Add(btnClose)
+        Controls.Add(btnLogin)
+        Controls.Add(txtPassword)
+        Controls.Add(txtUserID)
+        Controls.Add(lblPassword)
+        Controls.Add(lblUserID)
+        Name = "frmLoginScreen"
+        Text = "ログイン"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents lblUserID As Label
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents txtUserID As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnClose As Button
 End Class
--- Githubの動作確認（visual studio 2022）/Form1.vb
+++ Githubの動作確認（visual studio 2022）/Form1.vb
@@ -1,3 +1,5 @@
-﻿Public Class Form1
+﻿Public Class frmLoginScreen
+    Private Sub frmLoginScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 End Class
