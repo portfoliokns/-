b64c0aced324cb187ddf35d57134443374b93c3b
konishi33 <100762595+konishi33@users.noreply.github.com>
2023-04-08 23:37:02 +0900

    ログインフォームの名称変更に伴うエラーを修正

--- Githubの動作確認（visual studio 2022）/Form1.vb
+++ Githubの動作確認（visual studio 2022）/Form1.vb
@@ -2,4 +2,12 @@
     Private Sub frmLoginScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+
+    End Sub
 End Class
