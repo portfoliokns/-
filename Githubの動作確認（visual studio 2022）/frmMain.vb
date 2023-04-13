a5219ff276671c9364c75a0a5881afe899871f02
konishi33 <100762595+konishi33@users.noreply.github.com>
2023-04-13 10:53:25 +0900

    アカウント登録画面の表示機能を実装

--- Githubの動作確認（visual studio 2022）/frmMain.vb
+++ Githubの動作確認（visual studio 2022）/frmMain.vb
@@ -2,4 +2,9 @@
     Private Sub btnLogout_Click(sender As Object, e As EventArgs) Handles btnLogout.Click
         Me.Close()
     End Sub
+
+    Private Sub btnAddAccount_Click(sender As Object, e As EventArgs) Handles btnAddAccount.Click
+        Dim AddAccount As New frmAddAccount
+        AddAccount.Show()
+    End Sub
 End Class
